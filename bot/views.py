395cfcea55028e6935221576c22fe22212f9b38b
olegmir59 <udom1@ya.ru>
2025-09-09 19:24:29 +0700

    add views.py, serializers.py,urls.py

--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import TelegramUser
+from .serializers import TelegramUserSerializer
 
-# Create your views here.
+
+@api_view(['POST'])
+def register_user(request):
+    data = request.data
+    user, created = TelegramUser.objects.get_or_create(
+        user_id=data['user_id'],
+        defaults={'username': data.get('username', '')}
+    )
+    if created:
+        serializer = TelegramUserSerializer(user)
+        return Response(serializer.data)
+    else:
+        return Response({'message': 'User is already registered'})
--- djangobot/urls.py
+++ djangobot/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from bot.views import register_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/register/', register_user),
 ]
